b973c4071d3a6c8bd616358bb56a3bf4d9e4e118
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-01-16 16:01:25 +0300

    feat: add mobile view

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const rightContent = document.querySelector('.right-content');
 const skillsLine = document.querySelectorAll('.skills-line');
 const skillsBar = document.querySelectorAll('.skills-bar');
 const skillsWrapper = document.querySelector('.skills-wrapper');
+const burgerBtn = document.querySelector('.burger-btn');
+const menuBody = document.querySelector('.menu-body');
+const overlay = document.querySelector('.overlay');
+const navLinks = document.querySelectorAll('.nav-links');
 
 function showItem() {
   skillsLine.forEach(skillsLines => {
@@ -24,4 +28,39 @@ rightContent.addEventListener('scroll', () => {
   } else {
     hideItem();
   }
+})
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  menuBody.classList.toggle('active');
+  overlay.classList.toggle('active');
+})
+
+overlay.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  menuBody.classList.toggle('active');
+  overlay.classList.toggle('active');
+})
+
+const cardHover = document.querySelectorAll('.education-card');
+
+let hover = (e) => {
+  e.target.classList.add('hover');
+}
+
+let end = (e) => {
+  e.target.classList.remove('hover');
+}
+
+cardHover.forEach(card => {
+  card.addEventListener('touchmove', hover)
+  card.addEventListener('touchend', end)
+})
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    burgerBtn.classList.toggle('active');
+    menuBody.classList.toggle('active');
+    overlay.classList.toggle('active');
+  })
 })
